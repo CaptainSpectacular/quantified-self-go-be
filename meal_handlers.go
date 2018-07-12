4cf7929296e559d75fc238a949aee974b5e0a5b9
pollockcl <pollock.clee@gmail.com>
2018-07-12 09:35:24 -0600

    still cors

--- meal_handlers.go
+++ meal_handlers.go
@@ -8,6 +8,7 @@ import (
 
 func MealIndex(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+    (*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 
     meals := QueryMeals()
     json.NewEncoder(w).Encode(meals)
@@ -15,6 +16,7 @@ func MealIndex(w http.ResponseWriter, r *http.Request) {
 
 func MealShow(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+    (*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 
     id := mux.Vars(r)["id"]
     meal := QueryMeal(id)
@@ -33,6 +35,7 @@ func MealUpdate(w http.ResponseWriter, r *http.Request) {
 
 func MealDelete(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+    (*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 
     mid := mux.Vars(r)["mid"]
     fid := mux.Vars(r)["fid"]
