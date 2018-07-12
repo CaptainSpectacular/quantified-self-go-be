25e8caaa0dd3101365c982c717021e209bedae48
pollockcl <pollock.clee@gmail.com>
2018-07-12 09:36:38 -0600

    still cors

--- meal_handlers.go
+++ meal_handlers.go
@@ -8,7 +8,7 @@ import (
 
 func MealIndex(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-    (*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+    w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 
     meals := QueryMeals()
     json.NewEncoder(w).Encode(meals)
@@ -16,7 +16,7 @@ func MealIndex(w http.ResponseWriter, r *http.Request) {
 
 func MealShow(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-    (*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+    w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 
     id := mux.Vars(r)["id"]
     meal := QueryMeal(id)
@@ -25,6 +25,7 @@ func MealShow(w http.ResponseWriter, r *http.Request) {
 
 func MealUpdate(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+    w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 
     mid := mux.Vars(r)["mid"]
     fid := mux.Vars(r)["fid"]
@@ -35,7 +36,7 @@ func MealUpdate(w http.ResponseWriter, r *http.Request) {
 
 func MealDelete(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-    (*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+    w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 
     mid := mux.Vars(r)["mid"]
     fid := mux.Vars(r)["fid"]
