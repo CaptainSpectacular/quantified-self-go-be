12ece70da8f2ba3fe2c26970a8fb8a629f7a9c09
pollockcl <pollock.clee@gmail.com>
2018-07-12 09:40:26 -0600

    still cors

--- meal_handlers.go
+++ meal_handlers.go
@@ -16,7 +16,7 @@ func MealIndex(w http.ResponseWriter, r *http.Request) {
 
 func MealShow(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-    w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+    w.Header().Add("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 
     id := mux.Vars(r)["id"]
     meal := QueryMeal(id)
